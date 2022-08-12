a30b25adc74aba25a93582826c17be518874a4c1
Elmira Grigoryan <106161682+Elmira01@users.noreply.github.com>
2022-08-12 17:32:03 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,15 +39,15 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-    return morseCode
-    .split('   ')
-    .map(
-      a => a
-        .split(' ')
-        .map(
-          b => MORSE_TABLE[b]
-        ).join('')
-    ).join(' ');
+  return morseCode
+             .split("   ") // get word code 3 spaces apart
+             .map(word => word
+                           .split(" ") // get character code 1 spaces apart
+                           .map(character => MORSE_CODE[character]) // decode Morse code character
+                           .join('')
+              )
+              .join(' ') // add spaces between words 
+              .trim()
 }
 
 module.exports = {
